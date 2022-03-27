0c39ebb665bbb870bef127280547335f7901a952
Conor M <conor@hypetech.co.uk>
2022-03-27 12:03:43 +0100

    Added clickable tiles to bait minigame

--- minigames/Bait/BaitMatch.gd
+++ minigames/Bait/BaitMatch.gd
@@ -1,11 +1,19 @@
 extends Sprite
 
-var char_tex = load("res://minigames/Bait/black-square.png") 
+var char_tex = load("res://minigames/Bait/black-square.png")
+var black = true 
 
 func _ready():
 	set_process_input(true)
 	texture = char_tex
+	yield(get_tree().create_timer(5), "timeout")
+	get_tree().change_scene("res://Node2D.tscn")
 
 func _input(event):
-	if event is InputEventMouseButton:
-		texture = load("res://minigames/Bait/white-square.png")
+	if event.is_action_released("MouseUp"):
+		if black:
+			texture = load("res://minigames/Bait/white-square.png")
+		else:
+			texture = load("res://minigames/Bait/black-square.png")
+		black = not black
+		print(black)
